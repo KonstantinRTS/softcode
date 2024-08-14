0ce5150d90321e304e778f1d8aa618a130344aee
konstantinrts <k.developer@acceptance.ltd>
2024-08-14 14:52:33 +0300

    all files

--- js/main.js
+++ js/main.js
@@ -1,5 +1,16 @@
 $ = jQuery;
+
+$(window).on('load', function() {
+    // Плавно скрываем прелоадер после полной загрузки страницы
+    $('#preloader').fadeOut('slow', function() {
+        $(this).remove(); // Удаляем прелоадер из DOM после его скрытия
+    });
+});
+
+
 $(document).ready(function () {
+
+    new WOW().init();
 
     $('.marquee-slider-first').slick({
         dots: false,
@@ -26,6 +37,66 @@ $(document).ready(function () {
     });
 
     $('.marquee-slider-third').slick({
+        dots: false,
+        arrows: false,
+        infinite: true,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 0,
+        speed: 18000,
+        cssEase: 'linear'
+    });
+
+    $('.marquee-slider-fourth').slick({
+        dots: false,
+        arrows: false,
+        infinite: true,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 0,
+        speed: 18000,
+        cssEase: 'linear'
+    });
+
+    $('.marquee-slider-fifth').slick({
+        dots: false,
+        arrows: false,
+        infinite: true,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 0,
+        speed: 18000,
+        cssEase: 'linear'
+    });
+
+    $('.marquee-slider-sixth').slick({
+        dots: false,
+        arrows: false,
+        infinite: true,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 0,
+        speed: 18000,
+        cssEase: 'linear'
+    });
+
+    $('.marquee-slider-seventh').slick({
+        dots: false,
+        arrows: false,
+        infinite: true,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 0,
+        speed: 18000,
+        cssEase: 'linear'
+    });
+
+    $('.work-hero-marquee').slick({
         dots: false,
         arrows: false,
         infinite: true,
@@ -82,6 +153,17 @@ $(document).ready(function () {
             speed: 800,
         });
 
+        $('.team-welcome-slider').slick({
+            arrows: true,
+            appendArrows: '.team-welcome-slider__buttons',
+            prevArrow: '.team-welcome-slider__btn-prev',
+            nextArrow: '.team-welcome-slider__btn-next',
+            slidesToShow: 1,
+            dots: false,
+            speed: 800,
+        });
+
+
         $('.home-stack-slider').slick({
             arrows: true,
             appendArrows: '.home-stack-slider__buttons',
@@ -91,6 +173,38 @@ $(document).ready(function () {
             dots: false,
             speed: 800,
         });
+
+        $('.services-sol-slider').slick({
+            arrows: true,
+            appendArrows: '.services-sol-slider__buttons',
+            prevArrow: '.services-sol-slider__btn-prev',
+            nextArrow: '.services-sol-slider__btn-next',
+            slidesToShow: 2,
+            dots: false,
+            speed: 800,
+        });
+
+        $('.work-discover-slider').slick({
+            arrows: true,
+            appendArrows: '.work-discover-slider__buttons',
+            prevArrow: '.work-discover-slider__btn-prev',
+            nextArrow: '.work-discover-slider__btn-next',
+            slidesToShow: 1,
+            dots: false,
+            speed: 800,
+        });
+
+        
+        $('.career-our-slider').slick({
+            arrows: true,
+            appendArrows: '.career-our-slider__buttons',
+            prevArrow: '.career-our-slider__btn-prev',
+            nextArrow: '.career-our-slider__btn-next',
+            slidesToShow: 1,
+            dots: false,
+            speed: 800,
+        });
+        
     }
 
     /* Проверяю есть ли на странице такая секция и фиксирую её */
@@ -98,6 +212,52 @@ $(document).ready(function () {
 
         var $qwerty = $('._fixed-block');
         var $col01 = $('.home-welcome__col-01');
+        var col01Height = $col01.height();
+        var qwertyHeight = $qwerty.height();
+
+        $(window).scroll(function () {
+  
+            if (window.innerWidth > 992) {
+                var scrollPosition = $(this).scrollTop();
+
+                if (scrollPosition >= $col01.offset().top && scrollPosition <= ($col01.offset().top + col01Height - qwertyHeight)) {
+                    $qwerty.css({
+                        'position': 'fixed',
+                        'top': '0px',
+                        'left': 'auto',
+                        'bottom': 'auto'
+                    });
+                } else if (scrollPosition > ($col01.offset().top + col01Height - qwertyHeight)) {
+                    $qwerty.css({
+                        'position': 'absolute',
+                        'top': 'auto',
+                        'left': '0',
+                        'bottom': '0'
+                    });
+                } else {
+                    $qwerty.css({
+                        'position': 'static',
+                        'top': 'auto',
+                        'left': 'auto',
+                        'bottom': 'auto'
+                    });
+                }
+            } else {
+
+                $qwerty.css({
+                    'position': 'static',
+                    'top': 'auto',
+                    'left': 'auto',
+                    'bottom': 'auto'
+                });
+            }
+        });
+    }
+
+    if ($('.team-welcome__col-01').length > 0) {
+
+        var $qwerty = $('._fixed-block');
+        var $col01 = $('.team-welcome__col-01');
         var col01Height = $col01.height();
         var qwertyHeight = $qwerty.height();
 
